139ed57286c27acd14467b4a22c06df78a78b527
Marutipandey <pp874765.com>
2025-12-26 12:06:23 +0530

    nothings

--- news/src/components/Login.tsx
+++ news/src/components/Login.tsx
@@ -22,7 +22,7 @@ export default function Login() {
     setMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://backend-logic-beryl.vercel.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
